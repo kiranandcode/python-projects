5306e7a8d7a4ddc98186496cc16cb28f1141d6a4
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-04-07 23:06:17 +0100

    implemented more django stuff - starting to remember the api again. Yeah. it's slowly coming back

--- djangoRefresher/refresher/views.py
+++ djangoRefresher/refresher/views.py
@@ -1,12 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 # Create your views here.
+from django.template import loader
+
+from refresher.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello world! You're at the index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('refresher/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+
+#    return HttpResponse(template.render(context, request))
+    return render(request, 'refresher/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}".format(question_id))
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    return render(request, "refresher/detail.html", {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s." % question_id
