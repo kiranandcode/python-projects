282be5aff4fbfd0483df6f65d15efebbe5977c52
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-04-07 22:46:43 +0100

    implemented more django basic stuff - the faults of nodejs are becoming very hard to ignore now.

--- djangoRefresher/refresher/views.py
+++ djangoRefresher/refresher/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world! You're at the index")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s." % question_id
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
