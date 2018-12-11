365a95e4e5a45bcda59f2eefbf8afc8bbc6739e3
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-12-11 23:39:23 +0000

    begain messing arround with opencv

---
+++ video_analysis/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+import cv2
+
+
+# open video file
+cap = cv2.VideoCapture(sys.argv[1])
+
+while(True):
+    # read frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
